2415989db274f0223956dced96df861679151a80
AbdelrahmanKaled12 <abdelrahman.khaled1086@gmail.com>
2023-05-31 11:19:26 +0300

    add project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,11 +3,13 @@ import Title from "./Title";
 import imageProject1 from "../images/projects/project-1.png"
 import imageProject2 from "../images/projects/project-2.png"
 import imageProject3 from "../images/projects/project-3.png"
+import imageProject4 from "../images/projects/project-4.png"
 import html from "../images/icons/html.png"
 import css from "../images/icons/css.png"
 import js from "../images/icons/js.png"
 import wow from "../images/icons/wow-logo.png"
 import sweet from "../images/icons/swal2-logo.png"
+import owl from "../images/icons/owl.png"
 import bootstrap from "../images/icons/bootstrap.png"
 import emailjs from "../images/icons/emailjs.png"
 
@@ -58,6 +60,26 @@ function Portfolio() {
                         <div className="source d-flex">
                             <a href="https://abdelrahmankhaled12.github.io/Sleep-Station/" className="me-2">Live Demo <i className="fa-solid fa-laptop-code ms-1" /></a>
                             <a href="https://github.com/Abdelrahmankhaled12/Sleep-Station"> Repo <i className="fa-solid fa-code ms-1" /></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="box wow animate__fadeInUp  mt-5" data-wow-duration="2s">
+                    <img className="imageProject" src={imageProject4} alt="" />
+                    <div className="tools d-flex ms-2 flex-wrap">
+                        <img className="me-2 mt-2" src={html} alt="" />
+                        <img className="me-2 mt-2" src={css} alt="" />
+                        <img className="me-2 mt-2" src={js} alt="" />
+                        <img className="me-2 mt-2" src={bootstrap} alt="" />
+                        <img className="me-2 mt-2" src={wow} alt="" />
+                        <img className="me-2 mt-2" src={owl} alt="" />
+                    </div>
+                    <div className="flex d-flex justify-content-between align-items-center mt-2 ms-3 me-3">
+                        <a href="https://abdelrahmankhaled12.github.io/APEX/">
+                            <h4>APEX</h4>
+                        </a>
+                        <div className="source d-flex">
+                            <a href="https://abdelrahmankhaled12.github.io/APEX/" className="me-2">Live Demo <i className="fa-solid fa-laptop-code ms-1" /></a>
+                            <a href="https://github.com/Abdelrahmankhaled12/APEX"> Repo <i className="fa-solid fa-code ms-1" /></a>
                         </div>
                     </div>
                 </div>
