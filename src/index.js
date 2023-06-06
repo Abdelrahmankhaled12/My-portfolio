6d4ae0c510ccdd3474e40eae03eef9b21a4e3a0f
AbdelrahmanKaled12 <abdelrahman.khaled1086@gmail.com>
2023-06-06 21:22:11 +0300

    up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import './script';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
